ca63ea4dfc04f383ffc958ce0fccd048cab60ec9
Ritesh Chaudhary <riteshchaudhary63430@gmail.com>
2024-08-06 20:16:35 +0530

    fixed background

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,9 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-  },
-  plugins: [],
-};
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
-  theme: {
     extend: {
       backgroundImage: {
         "custom-image": "url('./public/bg.jpg')",
-        
       },
     },
   },
