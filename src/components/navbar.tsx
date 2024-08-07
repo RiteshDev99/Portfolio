36281d372955730a894f13a83719607cf4490e6e
Ritesh Chaudhary <riteshchaudhary63430@gmail.com>
2024-08-07 15:36:17 +0530

    logoSize fiexd

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -8,7 +8,9 @@ interface NavbarProps {
 const Navbar = ({ onHomeClick, onSkillsClick, onEducationClick, ...props }: NavbarProps) => {
     return (
         <div className="flex justify-between p-5 text-white absolute min-w-[100%]">
-            <img  src="public/pic.jpeg" className="h-10 w-10 rounded-full"/>
+            <span className="h-8 w-8 overflow-hidden">
+                <img src="public/pic.jpeg" className="h-full w-full rounded-full "/>
+            </span>
             <div className="flex gap-9 px-9 nav-elements ">
                 <button {...props} onClick={onHomeClick} className="hover:text-blue-500 font-bold">Home</button>
                 <button {...props} onClick={onSkillsClick} className="hover:text-blue-500 font-bold">Skills</button>
