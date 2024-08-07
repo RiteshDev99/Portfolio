ac6110b7d95c37b4e8645e3cd53e161f3d08adc2
Ritesh Chaudhary <riteshchaudhary63430@gmail.com>
2024-08-07 15:40:27 +0530

    logo fiexd

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = ({ onHomeClick, onSkillsClick, onEducationClick, ...props }: Navb
     return (
         <div className="flex justify-between p-5 text-white absolute min-w-[100%]">
             <span className="h-8 w-8 overflow-hidden">
-                <img src="public/pic.jpeg" className="h-full w-full rounded-full "/>
+                <img src="pic.jpeg" alt="Logo" className="object-cover w-full h-full rounded-full" />
             </span>
             <div className="flex gap-9 px-9 nav-elements ">
                 <button {...props} onClick={onHomeClick} className="hover:text-blue-500 font-bold">Home</button>
